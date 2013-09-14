9d8cca50d8e36dda25351511432367ec8ba82d2f
Andy Croll <andy@goodscary.com>
2013-09-14 19:31:21 +0100

    More output functions for a screening
    - improve docs too

--- lib/odeon_uk/screening.rb
+++ lib/odeon_uk/screening.rb
@@ -15,10 +15,37 @@ module OdeonUk
     # Public: Returns the Type of screening (3d, baby, kids, live)
     attr_reader :varient
 
+    # Public: Initialize a screening
+    #
+    # film_name   - String of the film name
+    # cinema_name - String of the cinema name on the Odeon website
+    # date        - String/Date representing the date of the screening
+    # time        - String representing the time of the screening (24 hour clock)
+    # varient     - String representing the type of showing (e.g. 3d/baby/live)
     def initialize(film_name, cinema_name, date, time, varient=nil)
       @cinema_name, @film_name, @varient = cinema_name, film_name, varient
       @when = Time.parse("#{date} #{time}")
     end
 
+    # Public: The Date of the screening
+    #
+    # Returns a Date
+    def date
+      @when.to_date
+    end
+
+    # Public: The string representation of the date of the screening
+    #
+    # Returns a String
+    def date_s
+      date.to_s
+    end
+
+    # Public: The string representation of the time of the screening
+    #
+    # Returns a String
+    def time_s
+      "#{@when.hour}:#{@when.min}"
+    end
   end
 end
--- test/lib/odeon_uk/screening_test.rb
+++ test/lib/odeon_uk/screening_test.rb
@@ -21,4 +21,28 @@ describe OdeonUk::Screening do
       screening.varient.must_equal '3d'
     end
   end
+
+  describe '#date' do
+    subject { OdeonUk::Screening.new('Iron Man 3', 'Brighton', '2013-09-12', '11:30', '3d').date }
+    it 'should return date of showing' do
+      subject.must_be_instance_of(Date)
+      subject.must_equal Date.new(2013, 9, 12)
+    end
+  end
+
+  describe '#date_s' do
+    subject { OdeonUk::Screening.new('Iron Man 3', 'Brighton', '2013-09-12', '11:30', '3d').date_s }
+    it 'should return date as a string' do
+      subject.must_be_instance_of(String)
+      subject.must_equal '2013-09-12'
+    end
+  end
+
+  describe '#time_s' do
+    subject { OdeonUk::Screening.new('Iron Man 3', 'Brighton', '2013-09-12', '11:30', '3d').time_s }
+    it 'should return time as a string' do
+      subject.must_be_instance_of(String)
+      subject.must_equal '11:30'
+    end
+  end
 end
