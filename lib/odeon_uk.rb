2dac86e7755d56d3e0b352b3c234063b7ae4e39a
Andy Croll <andy@goodscary.com>
2013-11-05 18:57:47 +0000

    include tzinfo/data

--- lib/odeon_uk.rb
+++ lib/odeon_uk.rb
@@ -1,9 +1,12 @@
 require 'httparty'
 require 'nokogiri'
 require 'tzinfo'
+require 'tzinfo/data'
+
+require_relative './odeon_uk/version'
 
 require_relative './odeon_uk/internal/film_with_screenings_parser'
-require_relative './odeon_uk/version'
+
 require_relative './odeon_uk/cinema'
 require_relative './odeon_uk/film'
 require_relative './odeon_uk/screening'
