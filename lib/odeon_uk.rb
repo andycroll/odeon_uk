e7ee076dcff1a449875110d372f1ba880fed4804
Andy Croll <andy@goodscary.com>
2014-09-19 16:05:14 +0200

    include files

--- lib/odeon_uk.rb
+++ lib/odeon_uk.rb
@@ -5,12 +5,14 @@ require 'tzinfo/data'
 require_relative './odeon_uk/version'
 
 require_relative './odeon_uk/internal/film_with_screenings_parser'
-require_relative './odeon_uk/internal/showtimes_parser'
+require_relative './odeon_uk/internal/showtimes_page'
+require_relative './odeon_uk/internal/title_sanitizer'
 require_relative './odeon_uk/internal/website'
 
 require_relative './odeon_uk/cinema'
 require_relative './odeon_uk/film'
 require_relative './odeon_uk/screening'
 
+# odeon_uk gem
 module OdeonUk
 end
