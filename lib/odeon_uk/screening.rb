b32b2c458839fd8d946bf8463745f0a2c08330cf
Andy Croll <andy@goodscary.com>
2014-09-19 16:08:18 +0200

    screenings have dimension

--- lib/odeon_uk/screening.rb
+++ lib/odeon_uk/screening.rb
@@ -1,40 +1,76 @@
 module OdeonUk
-
   # The object representing a single screening of a film on the Odeon UK website
   class Screening
+    FILM_CSS = '.film-detail'
 
     # @return [String] the booking URL on the cinema website
     attr_reader :booking_url
+    # @return [String] 2d or 3d
+    attr_reader :dimension
     # @return [String] the cinema name
     attr_reader :cinema_name
     # @return [String] the film name
     attr_reader :film_name
-    # @return [Time] the UTC time of the screening
-    attr_reader :when
-    # @return [String] the type of screening (2D, 3D, IMAX...)
-    attr_reader :variant
 
-    # @param [String] film_name the film name
-    # @param [String] the cinema name
-    # @param [Time] time datetime of the screening (UTC preferred)
-    # @param [String] booking_url direct link to the booking page for this screening
-    # @param [String] variant the type of showing (e.g. 3d/baby/live)
-    def initialize(film_name, cinema_name, time, booking_url=nil, variant=nil)
-      @cinema_name, @film_name, @variant = cinema_name, film_name, variant
-      @booking_url = booking_url
-      @when = time.utc? ? time : TZInfo::Timezone.get('Europe/London').local_to_utc(time)
+    # @param [Hash] options options hash
+    # @option options [String] :booking_url (nil) booking url for the screening
+    # @option options [String] :cinema_name name of the cinema
+    # @option options [String] :cinema_id website id of the cinema
+    # @option options [String] :dimension ('2d') dimension of the screening
+    # @option options [String] :film_name name of the film
+    # @option options [Time] :time utc time of the screening
+    # @option options [Array<String>] :variant ([]) type of screening
+    def initialize(options)
+      @booking_url = options.fetch(:booking_url, nil)
+      @cinema_name = options.fetch(:cinema_name)
+      @cinema_id   = options.fetch(:cinema_id)
+      @dimension   = options.fetch(:dimension, '2d')
+      @film_name   = options.fetch(:film_name)
+      @time        = options.fetch(:time)
+      @variant     = options.fetch(:variant, [])
+    end
+
+    # All currently listed films showing at a cinema
+    # @param [Integer] cinema_id id of the cinema on the website
+    # @return [Array<OdeonUk::Screening>]
+    def self.at(cinema_id)
+      showtimes_page(cinema_id).to_a.map do |html|
+        screenings_parser(html).to_a.map do |attributes|
+          new(attributes.merge(cinema_hash(cinema_id)))
+        end
+      end.flatten
     end
 
     # The date of the screening
     # @return [Date]
-    def date
-      @when.to_date
+    def showing_on
+      showing_at.to_date
     end
 
-    # @deprecated Please use {#variant} instead, I can't spell
-    def varient
-      warn "Please use #variant instead, I can't spell"
-      variant
+    # The UTC time of the screening
+    # @return [Time]
+    def showing_at
+      @time
+    end
+
+    # The kinds of screening
+    # @return <Array[String]>
+    def variant
+      @variant.map(&:downcase).sort
+    end
+
+    private
+
+    def self.screenings_parser(html)
+      OdeonUk::Internal::FilmWithScreeningsParser.new(html)
+    end
+
+    def self.showtimes_page(cinema_id)
+      OdeonUk::Internal::ShowtimesPage.new(cinema_id)
+    end
+
+    def self.cinema_hash(id)
+      { cinema_id: id, cinema_name: Cinema.find(id).name }
     end
   end
 end
--- test/lib/odeon_uk/screening_test.rb
+++ test/lib/odeon_uk/screening_test.rb
@@ -1,53 +1,87 @@
 require_relative '../../test_helper'
 
 describe OdeonUk::Screening do
+  let(:described_class) { OdeonUk::Screening }
+
+  let(:website) { Minitest::Mock.new }
 
   before { WebMock.disable_net_connect! }
 
-  describe '#new film_name, cinema_name, date, time, variant' do
-    it 'stores film_name, cinema_name & when (in UTC)' do
-      screening = OdeonUk::Screening.new 'Iron Man 3', 'Brighton', Time.parse('2013-09-12 12:00') # non-UTC time
-      screening.film_name.must_equal 'Iron Man 3'
-      screening.cinema_name.must_equal 'Brighton'
-      screening.when.must_equal Time.utc(2013, 9, 12, 11, 0)
-      screening.booking_url.must_equal nil
-      screening.variant.must_equal nil
+  describe '.at(cinema_id)' do
+    subject { described_class.at(71) }
+
+    before do
+      website.expect(:showtimes, brighton_showtimes_html, [71])
+      website.expect(:sitemap, sitemap_html)
     end
 
-    it 'stores variant if passed' do
-      screening = OdeonUk::Screening.new 'Iron Man 3', 'Brighton', Time.utc(2013, 9, 12, 11, 0), nil, '3d'
-      screening.film_name.must_equal 'Iron Man 3'
-      screening.cinema_name.must_equal 'Brighton'
-      screening.when.must_equal Time.utc(2013, 9, 12, 11, 0)
-      screening.booking_url.must_equal nil
-      screening.variant.must_equal '3d'
+    it 'returns an array of screenings' do
+      OdeonUk::Internal::Website.stub :new, website do
+        subject.must_be_instance_of(Array)
+        subject.each do |screening|
+          screening.must_be_instance_of(described_class)
+        end
+      end
     end
 
-    it 'stores booking_url if passed' do
-      screening = OdeonUk::Screening.new 'Iron Man 3', 'Brighton', Time.utc(2013, 9, 12, 11, 0), 'http://booking_url'
-      screening.film_name.must_equal 'Iron Man 3'
-      screening.cinema_name.must_equal 'Brighton'
-      screening.when.must_equal Time.utc(2013, 9, 12, 11, 0)
-      screening.when.must_equal Time.utc(2013, 9, 12, 11, 0)
-      screening.booking_url.must_equal 'http://booking_url'
-      screening.variant.must_equal nil
+    it 'returns correct number of screenings' do
+      OdeonUk::Internal::Website.stub :new, website do
+        subject.count.must_equal 214
+      end
     end
   end
 
-  describe '#date' do
-    subject { OdeonUk::Screening.new('Iron Man 3', 'Brighton', Time.utc(2013, 9, 12, 11, 0), '3d').date }
-    it 'should return date of showing' do
+  describe '#showing_on' do
+    subject { described_class.new(options).showing_on }
+
+    let(:options) do
+      {
+        film_name:   'Guardians of the Galaxy',
+        cinema_id:   71,
+        cinema_name: 'Odeon Brighton',
+        time:        Time.utc(2014, 9, 12, 11, 0),
+      }
+    end
+
+    it 'should return date of screening' do
       subject.must_be_instance_of(Date)
-      subject.must_equal Date.new(2013, 9, 12)
+      subject.must_equal Date.new(2014, 9, 12)
     end
   end
 
-  describe '#varient (DEPRECATED)' do
-    subject { screening.varient }
-    let(:screening) { OdeonUk::Screening.new('Iron Man 3', 'Brighton', Time.utc(2013, 9, 12, 11, 0), nil, '3d') }
+  describe '#variant' do
+    subject { described_class.new(options).variant }
 
-    it 'should return variant' do
-      subject.must_equal '3d'
+    let(:options) do
+      {
+        film_name:   'Guardians of the Galaxy',
+        cinema_id:   71,
+        cinema_name: 'Odeon Brighton',
+        time:        Time.utc(2014, 9, 12, 11, 0),
+        variant:     ['Kids', 'BABY']
+      }
     end
+
+    it 'is an alphabetically ordered array of lower-cased strings' do
+      subject.must_be_instance_of Array
+      subject.each do |tag|
+        tag.must_be_instance_of String
+      end
+      subject.must_equal %w(baby kids)
+    end
+  end
+
+  private
+
+  def brighton_showtimes_html
+    read_file('../../../fixtures/showtimes/brighton.html')
+  end
+
+  def read_file(filepath)
+    File.read(File.expand_path(filepath, __FILE__))
+  end
+
+  def sitemap_html
+    read_file('../../../fixtures/sitemap.html')
   end
 end
