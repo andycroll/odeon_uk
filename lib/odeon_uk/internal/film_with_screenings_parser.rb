a11e23f14f2adbda8566f54b252e248afdc0a0ac
Andy Croll <andy@goodscary.com>
2014-09-19 16:06:08 +0200

    breakup film/screenings parser
    uses private classes

--- lib/odeon_uk/internal/film_with_screenings_parser.rb
+++ lib/odeon_uk/internal/film_with_screenings_parser.rb
@@ -1,67 +1,144 @@
 module OdeonUk
-
   # Internal utility classes: Do not use
   # @api private
   module Internal
-
     # Parses a chunk of HTML to derive movie showing data
     class FilmWithScreeningsParser
+      NAME_CSS      = '.presentation-info h4 a'
+      SHOWTIMES_CSS = '.times-all.accordion-group'
 
       # @param [String] film_html a chunk of html
-      def initialize(film_html)
-        @nokogiri_html = Nokogiri::HTML(film_html)
+      def initialize(html)
+        @html = html
       end
 
       # The film name
       # @return [String]
       def film_name
-        name = @nokogiri_html.css('.presentation-info h4 a').children.first.to_s
-
-        # screening types
-        name = name.gsub /\s+[23][dD]/, '' #remove 2d or 3d from title
-        name = name.gsub 'Autism Friendly Screening -', '' # remove autism friendly
-
-        # special screenings
-        name = name.gsub 'ROH -', 'Royal Opera House:' # fill out Royal Opera House
-        name = name.gsub 'Met Opera -', 'Met Opera:' # fill out Met Opera
-        name = name.gsub /Cinemagic \d{1,4} \-/, '' # remove cinemagic stuff
-        name = name.gsub 'UKJFF -', '' # remove UK Jewish festival prefix
-        name = name.gsub 'NT Live -', 'National Theatre:' # National theatre
-        name = name.gsub 'National Theatre Live -', 'National Theatre:'
-        name = name.gsub /\(Encore.+\)/, '' # National theatre, remove encore
-        name = name.gsub 'Bolshoi -', 'Bolshoi:' # bolshoi ballet
-        name = name.gsub /\([Ll]ive\)/, '' # remove bolshoi-style live
-        name = name.gsub 'Globe On Screen:', 'Globe:' # globe
-        if name.match /RSC Live/
-          name = 'Royal Shakespeare Company: ' + name.gsub(/\- RSC Live \d{1,4}/, '')
-        end
-
-        name = name.squeeze(' ') # spaces compressed
-        name = name.gsub /\A\s+/, '' # remove leading spaces
-        name = name.gsub /\s+\z/, '' # remove trailing spaces
+        @film_name ||= TitleSanitizer.new(raw_film_name).sanitized
       end
 
-      # Showings
-      # @return [Hash]
-      # @example
-      #   {
-      #     "2D" => [[Time.utc, 'http://www...'], [Time.utc, 'http://www...']],
-      #     "3D" => [[Time.utc, 'http://www...']]
-      #   }
-      def showings
-        tz = TZInfo::Timezone.get('Europe/London')
-        @nokogiri_html.css('.times-all.accordion-group').inject({}) do |result, variant_node|
-          variant = variant_node.css('.tech a').text.gsub('in ', '').upcase
-
-          times_url = variant_node.css('.performance-detail').map do |screening_node|
-            [
-              tz.local_to_utc(Time.parse(screening_node['title'].match(/\d+\/\d+\/\d+ \d{2}\:\d{2}/).to_s + ' UTC')),
-              "http://www.odeon.co.uk#{screening_node['href']}"
-            ]
+      # array containing hashes of screening attributes
+      # @return [Array<Hash>]
+      def to_a
+        doc.css(SHOWTIMES_CSS).map do |node|
+          dimension_parser(node).map do |hash|
+            hash.merge(film_name: film_name)
           end
+        end.flatten
+      end
 
-          result.merge(variant => times_url)
+      private
+
+      def doc
+        @doc ||= Nokogiri::HTML(@html)
+      end
+
+      def raw_film_name
+        @raw_film_name ||= doc.css(NAME_CSS).children.first.to_s
+      end
+
+      def dimension_parser(node)
+        DimensionNodeParser.new(node).to_hash
+      end
+    end
+
+    # parses chunk of screenings for a particular screening dimension
+    class DimensionNodeParser
+      SCREENING_CSS = '.show'
+      TECH_CSS = '.tech a'
+
+      def initialize(node)
+        @node = node
+      end
+
+      # array containing hashes of screening attributes for dimension
+      # @return [Array<Hash>]
+      def to_hash
+        screening_hashes.map do |hash|
+          hash.merge(
+            dimension: dimension,
+            variant: add_imax(hash[:variant])
+          )
         end
+      end
+
+      private
+
+      def add_imax(original)
+        imax? ? "#{original} imax".strip : original
+      end
+
+      def dimension
+        tech.match(/[23]d/)[0]
+      end
+
+      def imax?
+        !!tech.match(/imax/)
+      end
+
+      def screening_hashes
+        screening_nodes.map { |node| ScreeningNodeParser.new(node).to_hash }
+      end
+
+      def screening_nodes
+        @node.css(SCREENING_CSS)
+      end
+
+      def tech
+        @tech ||= @node.css(TECH_CSS).text.gsub('in ', '').downcase
+      end
+    end
+
+    # parses a single screening
+    class ScreeningNodeParser
+      TIME_REGEX = %r(\d+/\d+/\d+ \d{2}\:\d{2})
+      DBOX_REGEX = /D-Box/
+
+      def initialize(node)
+        @node = node
+      end
+
+      def to_hash
+        {
+          booking_url: booking_url,
+          time:        utc_time,
+          variant:     variant
+        }
+      end
+
+      private
+
+      def booking_url
+        link_attr('href').to_s
+      end
+
+      def link_attr(attribute)
+        @node.css('a').attribute(attribute)
+      end
+
+      def info
+        @node.css('i').to_s
+      end
+
+      def time
+        Time.parse(time_string)
+      end
+
+      def time_string
+        link_attr('title').to_s.match(TIME_REGEX).to_s + ' UTC'
+      end
+
+      def tz
+        @tz ||= TZInfo::Timezone.get('Europe/London')
+      end
+
+      def utc_time
+        tz.local_to_utc(time)
+      end
+
+      def variant
+        info.match(DBOX_REGEX) ? 'd-box' : ''
       end
     end
   end
--- test/lib/odeon_uk/internal/film_with_screenings_parser_test.rb
+++ test/lib/odeon_uk/internal/film_with_screenings_parser_test.rb
@@ -1,171 +1,127 @@
 require_relative '../../../test_helper'
 
 describe OdeonUk::Internal::FilmWithScreeningsParser do
+  let(:described_class) { OdeonUk::Internal::FilmWithScreeningsParser }
 
   describe '#film_name' do
-    subject { OdeonUk::Internal::FilmWithScreeningsParser.new(film_html).film_name }
+    subject { described_class.new(film_html).film_name }
 
-    describe 'passed valid film html with simple name' do
-      let(:film_html) { read_film_html('about-time') }
+    describe 'passed first film html' do
+      let(:film_html) { read_film_html('brighton/film_first') }
 
-      it 'returns the film name' do
-        subject.must_equal('About Time')
+      it 'returns film name' do
+        subject.must_equal 'A Most Wanted Man'
       end
     end
 
-    describe 'passed valid film html with 2d name' do
-      let(:film_html) { read_film_html('star-trek-into-darkness-2d') }
+    describe 'passed last film html' do
+      let(:film_html) { read_film_html('brighton/film_last') }
 
-      it 'returns the film name' do
-        subject.must_equal('Star Trek Into Darkness')
-      end
-    end
-
-    describe 'passed valid film html with autism text' do
-      let(:film_html) { read_film_html('autism-friendly-planes') }
-
-      it 'returns the film name' do
-        subject.must_equal('Planes')
-      end
-    end
-
-    describe 'passed valid film html with ROH live screening' do
-      let(:film_html) { read_film_html('royal-opera-house-turandot') }
-
-      it 'returns the film name' do
-        subject.must_equal('Royal Opera House: Turandot 2013')
-      end
-    end
-
-    describe 'passed valid film html with Met Opera screening' do
-      let(:film_html) { read_film_html('met-opera-eugene-onegin') }
-
-      it 'returns the film name' do
-        subject.must_equal('Met Opera: Eugene Onegin 2013')
-      end
-    end
-
-    describe 'passed valid film html with Met Opera screening' do
-      let(:film_html) { read_film_html('cinemagic-echo-planet') }
-
-      it 'returns the film name' do
-        subject.must_equal('Echo Planet')
-      end
-    end
-
-    describe 'passed valid film html with UK Jewish Film Festival' do
-      let(:film_html) { read_film_html('ukjff-from-cable-street-to-brick-lane') }
-
-      it 'returns the film name' do
-        subject.must_equal('From Cable Street To Brick Lane')
-      end
-    end
-
-    describe 'passed valid film html with NT Encore' do
-      let(:film_html) { read_film_html('national-theatre-live-frankenstein') }
-
-      it 'returns the film name' do
-        subject.must_equal('National Theatre: Frankenstein')
-      end
-    end
-
-    describe 'passed valid film html with NT Encore' do
-      let(:film_html) { read_film_html('nt-live-war-horse') }
-
-      it 'returns the film name' do
-        subject.must_equal('National Theatre: War Horse')
-      end
-    end
-
-    describe 'passed valid film html with Bolshoi Ballet' do
-      let(:film_html) { read_film_html('bolshoi-spartacus-live') }
-
-      it 'returns the film name' do
-        subject.must_equal('Bolshoi: Spartacus')
-      end
-    end
-
-    describe 'passed valid film html with Globe' do
-      let(:film_html) { read_film_html('globe-on-screen-twelfth-night') }
-
-      it 'returns the film name' do
-        subject.must_equal('Globe: Twelfth Night')
-      end
-    end
-
-    describe 'passed valid film html with RSC' do
-      let(:film_html) { read_film_html('rsc-richard-ii') }
-
-      it 'returns the film name' do
-        subject.must_equal('Royal Shakespeare Company: Richard II')
+      it 'returns film name' do
+        subject.must_equal 'The Riot Club'
       end
     end
   end
 
-  describe '#showings' do
-    subject { OdeonUk::Internal::FilmWithScreeningsParser.new(film_html).showings }
+  describe '#to_a' do
+    subject { described_class.new(film_html).to_a }
 
-    describe 'passed valid film html' do
-      let(:film_html) { read_film_html('about-time') }
+    describe 'passed first film html' do
+      let(:film_html) { read_film_html('brighton/film_first') }
 
-      it 'returns an hash of variants => array of times' do
-        subject.must_be_instance_of Hash
-        subject.each do |key, value|
-          key.must_equal '2D'
-          value.must_be_instance_of Array
-          value.each do |element|
-            element[0].must_be_instance_of Time
-            element[0].zone.must_equal 'UTC'
-            element[1].must_be_instance_of String
-            element[1].must_match 'http://www.odeon.co.uk/'
-          end
+      it 'returns an array of hashes of screening attributes' do
+        subject.must_be_instance_of Array
+        subject.each do |hash|
+          hash[:booking_url].must_match 'https://www.odeon.co.uk/'
+          hash[:dimension].must_match(/[23]d/)
+          hash[:film_name].must_be_instance_of String
+          hash[:time].must_be_instance_of Time
+          hash[:time].zone.must_equal 'UTC'
+        end
+      end
+
+      it 'returns the same film title' do
+        first_title = subject[0][:film_name]
+        subject.each do |hash|
+          hash[:film_name].must_equal first_title
         end
       end
 
       it 'returns the correct number of screenings' do
-        subject['2D'].count.must_equal 21
-      end
-
-      it 'returns UTC times for showings' do
-        # about times are from British Summer Time UTC+1
-        # the actual show time is 20:30 from the fixture
-        subject['2D'].last[0].must_equal Time.utc(2013, 9, 19, 19, 30, 0)
+        subject.count.must_equal 10
       end
     end
 
-    describe 'passed valid film html for multi variants' do
-      let(:film_html) { read_film_html('thor-the-dark-world', 'manchester') }
+    describe 'passed last film html' do
+      let(:film_html) { read_film_html('brighton/film_last') }
 
-      it 'returns an hash of variants => array of times' do
-        subject.must_be_instance_of Hash
-        subject.keys.must_equal ['2D', 'IMAX 3D', '3D']
-        subject.values.each do |value|
-          value.must_be_instance_of Array
-          value.each do |element|
-            element[0].must_be_instance_of Time
-            element[0].zone.must_equal 'UTC'
-            element[1].must_be_instance_of String
-            element[1].must_match 'http://www.odeon.co.uk/'
-          end
+      it 'returns an array of hashes of screening attributes' do
+        subject.must_be_instance_of Array
+        subject.each do |hash|
+          hash[:booking_url].must_match 'https://www.odeon.co.uk/'
+          hash[:dimension].must_match(/[23]d/)
+          hash[:film_name].must_be_instance_of String
+          hash[:time].must_be_instance_of Time
+          hash[:time].zone.must_equal 'UTC'
         end
       end
 
-      it 'returns the correct number of screenings' do
-        subject['2D'].count.must_equal 1
-        subject['3D'].count.must_equal 1
-        subject['IMAX 3D'].count.must_equal 37
+      it 'returns at least 1' do
+        subject.count.must_be :>=, 1
+      end
+    end
+
+    describe 'passed imax film html' do
+      let(:film_html) { read_film_html('manchester/film_first_imax') }
+
+      it 'returns an array of hashes of screening attributes' do
+        subject.must_be_instance_of Array
+        subject.each do |hash|
+          hash[:booking_url].must_match 'https://www.odeon.co.uk/'
+          hash[:dimension].must_match(/[23]d/)
+          hash[:film_name].must_be_instance_of String
+          hash[:time].must_be_instance_of Time
+          hash[:time].zone.must_equal 'UTC'
+        end
       end
 
-      it 'returns UTC times for showings' do
-        # about times are from GMT
-        # the actual show time is 00:01 from the fixture
-        subject['2D'].last[0].must_equal Time.utc(2013, 10, 30, 0, 1, 0)
+      it 'contains imax in the variant' do
+        variants = subject.map { |hash| hash[:variant] }
+        variants.must_include('imax')
+      end
+
+      it 'returns at least 1' do
+        subject.count.must_be :>=, 1
+      end
+    end
+
+    describe 'passed d-box film html' do
+      let(:film_html) { read_film_html('liverpool_one/film_first_dbox') }
+
+      it 'returns an array of hashes of screening attributes' do
+        subject.must_be_instance_of Array
+        subject.each do |hash|
+          hash[:booking_url].must_match 'https://www.odeon.co.uk/'
+          hash[:dimension].must_match(/[23]d/)
+          hash[:film_name].must_be_instance_of String
+          hash[:time].must_be_instance_of Time
+          hash[:time].zone.must_equal 'UTC'
+        end
+      end
+
+      it 'contains d-box in the variant' do
+        variants = subject.map { |hash| hash[:variant] }
+        variants.must_include('d-box')
+      end
+
+      it 'returns at least 1' do
+        subject.count.must_be :>=, 1
       end
     end
   end
 
-  def read_film_html(filename, cinema='brighton')
-    path = "../../../../fixtures/#{cinema}-showtimes"
+  def read_film_html(filename)
+    path = '../../../../fixtures/showtimes'
     File.read(File.expand_path("#{path}/#{filename}.html", __FILE__))
   end
 end
