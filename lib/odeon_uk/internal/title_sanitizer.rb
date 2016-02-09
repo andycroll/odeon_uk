b9a73a9dd85a0cce9ced1ba94e25f15c873eab7e
Andy Croll <andy@goodscary.com>
2016-02-09 11:56:48 +0000

    TitleSanitizer inherits from cinebase

--- lib/odeon_uk/internal/title_sanitizer.rb
+++ lib/odeon_uk/internal/title_sanitizer.rb
@@ -3,49 +3,43 @@ module OdeonUk
   # @api private
   module Internal
     # Sanitize and standardize film titles
-    class TitleSanitizer
+    class TitleSanitizer < Cinebase::TitleSanitizer
+      # @!method initialize(title)
+      #   @param [String] title a film title
+      #   @return [CineworldUk::Internal::TitleSanitizer]
+
+      # @!method sanitized
+      #   sanitized and standardized title
+      #   @return [String] sanitised title
+
+      private
+
       # strings and regex to be removed
-      REMOVE = [
-        /\s+[23][dD]/,                 # dimension
-        'Autism Friendly Screening -', # autism screening
-        /\ACinemagic \d{1,4} \-/,      # cinemagic
-        /\(encore.+\)/i,               # encore for NT Live
-        /\(live\)/i,                   # live in brackets
-        'UKJFF -',                     # UK Jewish festival prefix
-        /\bsing\-?a\-?long\b/i,        # singalong
-        'Autism Friendly - ',          # autism friendly
-        'amp;',                        # html ampersands
-      ]
-
-      # regexes and their replacements
-      REPLACE = {
-        /Bolshoi - (.*)/               => 'Bolshoi: ',
-        /Globe On Screen: (.*)/        => 'Globe: ',
-        /Met Opera - (.*)/             => 'Met Opera: ',
-        /National Theatre Live - (.*)/ => 'National Theatre: ',
-        /NT Live - (.*)/               => 'National Theatre: ',
-        /ROH - (.*)/                   => 'Royal Opera House: ',
-        /(.*)\- RSC Live \d{1,4}/      => 'Royal Shakespeare Company: '
-      }
-
-      # @param [String] title a film title
-      def initialize(title)
-        @title = title
+      def remove
+        [
+          /\s+[23][dD]/,                 # dimension
+          'Autism Friendly Screening -', # autism screening
+          /\ACinemagic \d{1,4} \-/,      # cinemagic
+          /\(encore.+\)/i,               # encore for NT Live
+          /\(live\)/i,                   # live in brackets
+          'UKJFF -',                     # UK Jewish festival prefix
+          /\bsing\-?a\-?long\b/i,        # singalong
+          'Autism Friendly - ',          # autism friendly
+          'amp;',                        # html ampersands
+        ]
       end
 
-      # sanitized and standardized title
-      # @return [String] title
-      def sanitized
-        @sanitzed ||= begin
-          sanitized = @title
-          REMOVE.each do |pattern|
-            sanitized.gsub! pattern, ''
-          end
-          REPLACE.each do |pattern, prefix|
-            sanitized.gsub!(pattern) { |_| prefix + $1 }
-          end
-          sanitized.squeeze(' ').strip
-        end
+      # regexes and their replacements
+      def replace
+        {
+          /Bolshoi - (.*)/               => 'Bolshoi: ',
+          /Globe On Screen: (.*)/        => 'Globe: ',
+          /Met Opera - (.*)/             => 'Met Opera: ',
+          /National Theatre Live - (.*)/ => 'National Theatre: ',
+          /NT Live - (.*)/               => 'National Theatre: ',
+          /ROH - (.*)/                   => 'Royal Opera House: ',
+          /(.*)\- RSC Live \d{1,4}/      => 'Royal Shakespeare Company: '
+        }
       end
     end
   end
