e9d0a43e16e445bbd144632992be8a16bedfc89f
Andy Croll <andy@goodscary.com>
2016-02-09 12:44:24 +0000

    Allow net connects for Code Climate
    Plus test helper tidy up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,13 @@ CodeClimate::TestReporter.start
 
 require 'minitest/autorun'
 require 'minitest/reporters'
-reporter_options = { color: true, slow_count: 5 }
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+Minitest::Reporters.use! [
+  Minitest::Reporters::DefaultReporter.new(color: true, slow_count: 5)
+]
 
 require 'webmock/minitest'
+WebMock.allow_net_connect!
 
 require File.expand_path('../../lib/odeon_uk.rb', __FILE__)
 
